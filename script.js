b70a721eaea67342890c1d764e44fac00f5f2a81
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-07-05 23:40:34 +0530

    Working Quote

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 //get quote from API
-async function getQuote(){
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0?method=getQuote&lan=en&format=json';
-    try{
-        const response = await fetch(proxyUrl+apiUrl);
+async function getQuote() {
+    // loading();
+    // We need to use a Proxy URL to make our API call in order to avoid a CORS error
+    const proxyUrl = 'https://jacinto-cors-proxy.herokuapp.com/';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
-    }catch(err){
-        getQuote();
-        console.log('SomeThing Went to wrong',err);
+        // Check  Author field is blank
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //  font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop Loading, Show Quote
+        complete();
+    } catch (error) {
+        
     }
 }
+
+
+//For Twitter
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//for Event
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // onLoad
 getQuote();
